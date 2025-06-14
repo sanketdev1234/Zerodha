21ac0593b097c02f2d959fbe5fc49fa43024c4ac
sanketdev1234 <codetech28@gmail.com>
2025-06-14 09:06:44 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,19 @@ const mongoose = require("mongoose");
 var methodOverride = require('method-override'); 
 app.use(methodOverride('_method'));
 
+const cors = require('cors')
 const port=8080;
 const dburl=process.env.ATLAS_DBURL;
 
 
+const Holding=require("./model/HoldingsSchema.js");
+const Position=require("./model/PositionSchema.js");
+const Ordering=require("./model/OrdersSchema.js");
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
+
+app.use(cors());
 
 main()
   .then(() => {
@@ -22,17 +29,30 @@ main()
   .catch((err) => console.log(err));
 
 //   // connecting the mongo db with the localhost
-// async function main() {
-//   await mongoose.connect("mongodb://localhost:27017/Zerodha");
-// }
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/Zerodha");
+}
 
 
 // // connecting the mongo database with the mongoatlas cloud servive
-async function main() {
-  await mongoose.connect(dburl);
-}
+// async function main() {
+//   await mongoose.connect(dburl);
+// }
 
 app.listen(port , ()=>{
     console.log(`server is running on port ${port}`);
 });
+
+app.get("/getholding",async(req,res)=>{
+const holding=await Holding.find();
+  res.send({holding});
+})
+app.get("/getorder",async(req,res)=>{
+const order=await Ordering.find();
+  res.send({order});
+})
+app.get("/getposition",async(req,res)=>{
+const position=await Position.find();
+  res.send({position });
+})
 
