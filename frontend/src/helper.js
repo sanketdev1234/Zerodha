147a7bda04a6b2a67f609e00169a0edbfde82409
sanketdev1234 <codetech28@gmail.com>
2025-07-01 11:06:34 +0530

    user

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const authurl = `${import.meta.env.VITE_API_URL || 'http://localhost:8080'}api/auth/authstatus`;
+// const authurl = `${import.meta.env.VITE_API_URL || 'http://localhost:8080'}api/auth/authstatus`;
 
 const user_present = async () => {
   try {
-    const response = await axios.get(authurl, { withCredentials: true });
+    const response = await axios.get('api/auth/authstatus', { withCredentials: true });
     return response.data;
   } catch (err) {
     console.log("Authentication error:", err);
